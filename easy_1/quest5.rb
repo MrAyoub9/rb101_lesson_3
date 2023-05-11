142c602985d21ad104fe2d6ca79e73093df76279
Ayoub <fang.ayoub@gmail.com>
2023-05-11 11:45:39 -0400

    updated the answer of question 5

--- easy_1/quest5.rb
+++ easy_1/quest5.rb
@@ -3,10 +3,18 @@
     -> To find a number in a range we can:
         - use the iterators or loops
         - use Range#include?(obj) method which returns true if the obj is found
+        - use Rnage#cover?(obj) returns true if the object exist within the range
 
   => Method Range#include?(obj) returns true if the object is within the range
     - Otherwise, it will return false
-    - works on letters as well => ("a".."z").include?("c")  ==> true
+    - works on letters as well
+        => ("a".."z").include?("c")  ==> true
+        => ("a".."z").include?("cc")  ==> false
+
+  => Method Range#cover?(object) returns true if the object exist within the range
+      - works on letters as well
+        => ("a".."z").include?("c")  ==> true
+        => ("a".."z").include?("cc")  ==> true
 =end
 
 found = 'Not Found'
@@ -17,19 +25,21 @@ for i in (10..100)
     break
   end
 end
-
 puts found
 
 found = 'Not Found'
 # second way
 (10..100).each { |n| found = "Found" if n == 2}
-
 puts found
 
 found = 'Not Found'
 # Third and easiest way to find the number in a ranger
 found = "Found" if (10..100).include?(42)
+puts found
 
+#Another method
+found = 'Not Found'
+found = "Found" if (10..100).cover?(42)
 puts found
 
 # While/Loop do/ Until can all be used
