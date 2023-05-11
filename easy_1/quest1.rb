082a3a5929fb0eebe55b9992cbd7ab9a090364c5
Ayoub <fang.ayoub@gmail.com>
2023-05-11 11:36:41 -0400

    corrected answer of question 3 and added results of question 1

--- easy_1/quest1.rb
+++ easy_1/quest1.rb
@@ -1,13 +1,31 @@
 =begin
+
 The method #uniq deletes all duplicate values and return a new array.
 However, #uniq is not a destructive method and calling it on the array
 will not change or modify its value, but return a new array.
 To mutate the caller, use the method #uniq! which is destructive
+
+p object = is the same as = puts = object.inspect
 =end
 
 numbers = [1,2,2,3]
 numbers.uniq
-p numbers
+puts numbers
+
+=begin
+Print out:
+1
+2
+2
+3
+=end
+
 
 numbers.uniq!
-p numbers
+puts numbers
+=begin
+Print out:
+1
+2
+3
+=end
--- easy_1/quest3.rb
+++ easy_1/quest3.rb
@@ -1,16 +1,13 @@
 advice = "Few things in life are as important as house training your pet dinosaur."
-advice.tr_s!("important", "urgent")
+p advice
+advice.gsub!("important", "urgent")
 p advice
 
 =begin
 => There are few methods that can be used to replace strings or part of them:
-  => #TR/TR_S   Translation method - takes 2 args
-  - #tr / #tr! => replace the given string by the second argument. 
-    if the given string is longer the the new one, the new string will be padded with 
-    its last character to maintain the correspondence
+  => The method gsub!(str/pattern, new_str) also does the same job. can take string or a pattern
+      and replace it with the new string that was provided within the arguments
 
-  - #tr_s / #tr_s! This does almost the same job as #tr, but removes the duplicate character
-    in regions that were affected by translation method
   => #REPLACE - takes one aregument
   - #replace. This replace the whole string by the given argument. Mutating method
 =end
