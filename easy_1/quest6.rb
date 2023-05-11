4cd324d6f4bc77e0115edd1738c510df5fb656e1
Ayoub <fang.ayoub@gmail.com>
2023-05-11 11:55:28 -0400

    Updated the answer to question 6

--- easy_1/quest6.rb
+++ easy_1/quest6.rb
@@ -2,30 +2,27 @@
   => To do this we should use a mutating method and here are two options:
     -> Method 1: Str <<  Obj - this appends the given Obj to the Str
         - if a number is provided it would be converted to character
-    -> Method 2: str.concat(obj) - cancatenates the given obj to str
-        - numbers converted to characters before concatenation
+    -> Method 2: str.prepend(obj1, obj2...) adds a string at the beginning of the given string
+       => #prepend(str1, str2, ...) is a mutating method
+
+  => To add a string at the end of another string, use:
+      => str.concat(obj) - cancatenates the given obj to str
+      - numbers converted to characters before concatenation
 =end
 
 famous_words = "seven years ago..."
 
 def add_string1(msg)
-  msg.concat("Four score and")
+  msg.prepend("Four score and ")
 end
 
-add_string1(famous_words)
+famous_words = add_string1(famous_words)
 puts famous_words
 
 famous_words = "seven years ago..."
 def add_string2(msg)
-  msg << "Four score and"
+  "Four score and " << msg
 end
 
-add_string2(famous_words)
-puts famous_words
-
-
-# There is a method that adds a string at the beginning of the given string
-# The method is #prepend(str1, str2, ...) and it is a mutating method
-
-famous_words.prepend("1. ")
+famous_words = add_string2(famous_words)
 puts famous_words
