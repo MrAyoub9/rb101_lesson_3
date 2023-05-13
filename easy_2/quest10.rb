63cb663c36c4d5807b3f4b6c4d3d78dc9572093d
Ayoub <fang.ayoub@gmail.com>
2023-05-12 21:28:31 -0400

    Added another method to answer the same question String#center(width,padstr='')

--- easy_2/quest10.rb
+++ easy_2/quest10.rb
@@ -1,7 +1,21 @@
-title = "Flinstone Family Members"
+=begin
+  => The method #center takes two argument. the first argument represent the width of the string
+   and the second argument will include the text that will fill the empty area between the caller.
+   - the second argument is optional and if not provided, spaces will automatically be added to center the caller.
 
-while title.size < 40
-  title.prepend(" ").concat(" ")
+=end
+
+title = "Flinstone Family Members"
+another_string = "Flinstone Family Members"
+
+p title.center(40)
+
+# My Method
+def center_str(str, width)
+  while str.size < width
+    str.prepend(" ").concat(" ")
+  end
 end
 
-p title
+center_str(another_string, 60)
+p another_string
