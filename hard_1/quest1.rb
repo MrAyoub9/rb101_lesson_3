323a682673a4cb1c686063035e0ca7ac35683eea
Ayoub <fang.ayoub@gmail.com>
2023-05-15 16:33:06 -0400

    Updated comment

--- hard_1/quest1.rb
+++ hard_1/quest1.rb
@@ -1,9 +1,9 @@
 =begin
-    => if greeting was not initialized prior to if statement, the greeting variable will be holding
-      a nill value, because, it does not hold any value or in another word, the address memory to
-      which it points does not store any value.
+    => if greeting was not initialized prior to if statement, but initialized within if-statement
+      the greeting variable will be holding a nill value, because the variable was initialized even
+      if that if-statement was not executed.
     => the if-statement will be never executed because the condition is always false and this will
-      never allow re-assigning a new value to greeting, if it was initialized prior to if-statement
+      never allow re-assigning that value to greeting.
 =end
 
 
