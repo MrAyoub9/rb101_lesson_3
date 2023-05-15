c878e47779a62af5f249989ab5210a49027bc789
Ayoub <fang.ayoub@gmail.com>
2023-05-15 11:10:14 -0400

    Updated the comment

--- medium_2/quest3.rb
+++ medium_2/quest3.rb
@@ -1,8 +1,13 @@
 =begin
   => string_arg_one will not be mutated and will print the original value "pumpkins"
       - += does not mutate the caller and return a new string instead of modifying the original one.
+      - += creates a new String object and assign it to the local variable of the method which will be 
+            pointing to a different value than the one passed as an argument. The passed variable will not
+            be modified.
   => string_arg_two will be mutated and will print the value "pumpkinsrutabaga"
-      - << mutate the caller and add the given value to the original variable that reference the value pumpkins 
+      - << mutate the caller and add the given value to the original variable that reference the value pumpkins
+      - << mutates the object referenced by the passed argument, and the local variable will be pointing to the
+        same value as the one passed as an argument
 =end
 
 def tricky_method(string_param_one, string_param_two)
