832dfac2f1791a2c9eb3a1b9acaab2ee94855e18
Ayoub <fang.ayoub@gmail.com>
2023-05-15 11:15:51 -0400

    Updated the comment and added an answer

--- medium_2/quest6.rb
+++ medium_2/quest6.rb
@@ -1,5 +1,6 @@
 =begin
-  => The easiest way to do this, is using the conditinal ternary operator
+  => The easiest way to do this, to check if the conditions are true and return true/false
+  => Another way is using the conditinal ternary operator
   => It is also possible to make one line if-statement that returns true if conditions are true
     - otherwise, the last line of the method will return false
 =end
@@ -21,14 +22,22 @@ def color_valid2(color)
   color == "blue" || color == "green" ? true : false
 end
 
+def color_valid3(color)
+  color == "blue" || color == "green"
+end
+
 puts "Using the original method #color_valid"
 p color_valid("blue")
 p color_valid("red")
 
-puts "Using the original method #color_valid1"
+puts "Using the method #color_valid1"
 p color_valid1("blue")
 p color_valid1("red")
 
-puts "Using the original method #color_valid2"
+puts "Using the method #color_valid2"
+p color_valid2("blue")
+p color_valid2("red")
+
+puts "Using the method #color_valid3"
 p color_valid2("blue")
 p color_valid2("red")
